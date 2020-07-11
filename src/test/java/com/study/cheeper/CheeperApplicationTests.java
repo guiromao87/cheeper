82e613ad7b2637d5bb0edacc4bfb7e91890cf6c6
Guilherme Romão <guilhermersimoes87@gmail.com>
2020-07-11 09:31:03 -0300

    initial setup

---
+++ src/test/java/com/study/cheeper/CheeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.cheeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
