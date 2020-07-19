233548a8c19f745a51d9faf3ad748fb23dd105ad
Guilherme Romão <guilhermersimoes87@gmail.com>
2020-07-19 12:00:43 -0300

    show logged user at home with tag security thymeleaf

--- src/main/java/com/study/cheeper/controller/HomeController.java
+++ src/main/java/com/study/cheeper/controller/HomeController.java
@@ -1,13 +1,19 @@
 package com.study.cheeper.controller;
 
+import org.slf4j.Logger;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/home")
-    public String home() { return "/home"; }
+    public String home() {
+        return "/home";
+    }
 
     @GetMapping("/")
     public String home2() { return "/home"; }
