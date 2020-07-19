c26979f2b0a1c2f94eab0dec8c94c8418d045afb
Guilherme Romão <guilhermersimoes87@gmail.com>
2020-07-19 18:28:23 -0300

    Listing cheeps for user

--- src/main/java/com/study/cheeper/controller/HomeController.java
+++ src/main/java/com/study/cheeper/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.study.cheeper.controller;
 
 import com.study.cheeper.model.User;
+import com.study.cheeper.repository.CheepRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +11,18 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 
+    @Autowired
+    private CheepRepository cheepRepository;
+
     @GetMapping("/home")
     public ModelAndView home() {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return new ModelAndView("/home").addObject("autor",user);
+
+        ModelAndView mv = new ModelAndView("/home");
+        mv.addObject("autor",user);
+        mv.addObject("cheep", cheepRepository.findAll());
+
+        return mv;
     }
 
     @GetMapping("/")
