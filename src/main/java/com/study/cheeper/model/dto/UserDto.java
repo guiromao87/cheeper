21e0a323f7f89c1c3df6f2e706a70a070c8bd07c
Guilherme Romão <guilhermersimoes87@gmail.com>
2020-07-24 20:43:46 -0300

    create userDto

---
+++ src/main/java/com/study/cheeper/model/dto/UserDto.java
@@ -0,0 +1,30 @@
+package com.study.cheeper.model.dto;
+
+import com.study.cheeper.model.User;
+
+public class UserDto {
+
+    private Integer id;
+    private String name;
+    private String bio;
+    private String image;
+    private int numberOfCheeps;
+
+    public UserDto(User user) {
+        this.id = user.getId();
+        this.name = user.getName();
+        this.bio = user.getBio();
+        this.image = user.getImage();
+        this.numberOfCheeps = user.getNumberOfCheeps();
+    }
+
+    public Integer getId() { return id; }
+
+    public String getName() { return name; }
+
+    public String getBio() { return bio; }
+
+    public String getImage() { return image; }
+
+    public int getNumberOfCheeps() { return numberOfCheeps; }
+}
